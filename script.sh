8e6b5a42763012840156730be5c76734421e2c05
Abi (She/her) <91515654+awhittle2@users.noreply.github.com>
2023-05-02 08:59:03 -0700

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+echo "Checking if snap is installed"
 if command -v snap >/dev/null 2>&1; then
-	echo "Snap is installed"
+	echo "Snap is already installed"
 else
+	echo "Installing snap"
 	sudo apt install snapd
 fi
 
-pip install yq
-
-
-if ! command -v yq >/dev/null 2>&1; then
-	echo "yq did not install properly"
-	exit 1
+echo "Checking if yq (a yaml parser) is installed"
+if command -v yq >/dev/null 2>&1; then
+	echo "Installing yq"
+	pip install yq
+else
+	echo "Yq is already installed"
 fi
 
+# Yaml file with packages to install
 yaml_file="packages.yml"
 
+# Make sure everything is up to date
 sudo apt update
 
+# Loop through and install each package
 for package in $(yq e '.packages[]' "$yaml_file"); do
-	echo "Checking if its installed"
+	echo "Checking if $package is installed"
 	if dpkg -s "$package" >/dev/null 2>&1; then
-		echo "Already installed"
+		echo "$package is already installed"
 	else
 		echo "Installing $package..."
 		sudo apt install -y "$package"
